d5957827e7bd7ca849f72ac6cfa1b728718d93e3
Federico Silveri <federico.silveri@cazoo.co.uk>
2022-05-06 10:22:32 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/example.test.ts

--- src/main/example.ts
+++ src/main/example.ts
@@ -2,4 +2,8 @@ export class Example {
     add(a: number, b: number) {
         return a + b;
     }
+
+    fizzBuzz(number: number): any {
+        return "1";
+    }
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,18 @@
-import {Example} from "../main/example";
+import { Example } from "../main/example";
 
 describe('example test', () => {
     it("should give sum of a + b when adding a and b", () => {
         let example: Example = new Example();
         expect(example.add(1, 2)).toBe(3);
     })
-})// hello frederico
+})
+
+
+describe('Fizz Buzz Kata', () => {
+    it("it should print the given number as a string", () => {
+        let example: Example = new Example();
+        expect(example.fizzBuzz(1)).toBe("1");
+    })
+})
+
+
